d1bb3e240c3889f20b1e2f067dbf8a8cedeb6f25
git <dev3@melle.com>
2020-10-06 12:32:27 +0200

    Fix LaravelServiceProvider signleton resolver

--- src/LaravelServiceProvider.php
+++ src/LaravelServiceProvider.php
@@ -13,7 +13,13 @@ class LaravelServiceProvider extends ServiceProvider
         $this->app->singleton(GoogleGeocodeApi::class, function ($app) {
             $key = isset($app['config']['google-geocode.key'])
                 ? $app['config']['google-geocode.key'] : null;
-            return new GoogleGeocodeApi($key);
+            $verifySsl = isset($app['config']['google-geocode.verify_ssl'])
+                ? $app['config']['google-geocode.verify_ssl']
+                : true;
+            $headers = isset($app['config']['google-geocode.headers'])
+                ? $app['config']['google-geocode.headers']
+                : [];
+            return new GoogleGeocodeApi($key, $verifySsl, $headers);
         });
 
         $this->mergeConfigFrom(__DIR__ . "/../config/google-geocode.php", "google-geocode");
